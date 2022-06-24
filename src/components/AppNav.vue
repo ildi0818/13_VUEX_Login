d7540a3cb7a92eb47431a12ec8b476b65a883f8e
ildi0818 <ildikoacsnesulyok@gmail.com>
2022-06-24 17:52:27 +0200

    V1.0

---
+++ src/components/AppNav.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="container-fluid nav-bg-color">
+    <div class="container">
+      <nav class="navbar navbar-expand-lg navbar-light nav-bg-color">
+        <div class="container-fluid">
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item"
+              v-for="(menuItem,index) in menuItems"
+              :key="index">
+                <router-link class="nav-link" :to="menuItem.to">{{ menuItem.title }}</router-link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app-nav",
+  data(){
+    return{
+        menuItems:[
+            {title:'Nyitó oldal', to:'/'},
+            {title:'Bejelentkezés', to:'/bejelentkezes'},
+            {title:'Profil', to:'/profil'},
+            {title:'Kijelentkezés', to:'/kijelentkezes'},
+        ]
+    }
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="row">
+    <div class="col-12">
+        <h1 class="display-4 text-center my-5">Bejelentkezés</h1>
+    </div>
+
+    <div class="col-12 col-md-4 mx-auto">
+        
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+name:'login-view'
+}
+</script>
+
+<style>
+
+</style>
